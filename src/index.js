4451d3f5bf6ba9695bfa25299592057569d58d6e
daisukehanamura <yuu_dai_suke@yahoo.co.jp>
2024-08-17 15:00:25 +0900

    Create React App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import MyButton from './Study/test';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+
+const test = ReactDOM.createRoot(document.getElementById('test'));
+test.render(
+  <React.StrictMode>
+    <MyButton />
   </React.StrictMode>
 );
 
